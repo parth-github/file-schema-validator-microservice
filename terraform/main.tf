99e5cf430055e5058f45cfd1947ae0144b53d5ca
Smriti <linuxriti@gmail.com>
2025-06-14 17:19:04 +0530

    Required version added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,15 @@
 # Main Terraform module
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 0.12"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
