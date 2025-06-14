e2b79bff3e5263c142de35a0a4a4660344629509
Smriti <linuxriti@gmail.com>
2025-06-14 17:39:27 +0530

    Network Config removed main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,10 +55,4 @@ resource "aws_ecs_service" "fastapi_service" {
   launch_type     = "FARGATE"
   desired_count   = 1
 
-
-  network_configuration {
-    subnets          = ["subnet-xxxxxxxx"]
-    assign_public_ip = true
-    security_groups  = ["sg-xxxxxxxx"]
-  }
 }
