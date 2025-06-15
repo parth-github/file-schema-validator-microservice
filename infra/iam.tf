8c89f93f74d75841cc5b7f9e883cc9cfd576dc15
Smriti <linuxriti@gmail.com>
2025-06-15 12:17:47 +0530

    FSVM-42: S3 read access > policy > ecs-task-role >  task-definition

---
+++ infra/iam.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsAppS3AccessRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_read_policy" {
+  name = "ecsTaskS3ReadPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["s3:GetObject", "s3:ListBucket"],
+      Resource = [
+        "*"
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_read" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.s3_read_policy.arn
+}
--- infra/main.tf
+++ infra/main.tf
@@ -19,6 +19,7 @@ resource "aws_ecs_task_definition" "fastapi_task" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
